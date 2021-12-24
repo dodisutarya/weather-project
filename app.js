a8d21710e46d78504decfb051eb49388bd2c5c12
dodisutarya <ikhwands@protonmail.ch>
2021-12-24 15:06:19 +0700

    using axios

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express =require("express");
-const https = require("https");
+const axios = require("axios");
+const { response } = require("express");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.get("/", function(req, res){    
 
     const url = "http://api.openweathermap.org/data/2.5/weather?q=bandung&appid=2365189b3f75762ee973ac62dd28b36f&units=metric";
-    https.get(url, function(response){
-        console.log(response);
+    
+    axios.post(url)
+    .then(function(response){
+        console.log(response.data.weather[0].description);
     })
+
+    
+
+
+    
 
     res.send("Server is up and running.");
 })
