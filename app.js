32b2cdb0637b479cbc0619277e4a6dc452863932
dodisutarya <ikhwands@protonmail.ch>
2021-12-28 08:20:17 +0700

    render to browser live data api weather

--- app.js
+++ app.js
@@ -10,15 +10,19 @@ app.get("/", function(req, res){
     
     axios.post(url)
     .then(function(response){
-        console.log(response.data.weather[0].description);
+
+        const weatherDesc = response.data.weather[0].description;
+        const temp = response.data.main.temp;
+        const location = response.data.name;
+        const icon = response.data.weather[0].icon;
+        const urlicon = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+ 
+        res.write("<p>The weather in " + location + " curently is " + weatherDesc + "</p>");
+        res.write("<h1>The temperature in " + location + " is " + temp + "</h1>");
+        res.write("<img src='"+ urlicon + "'>");
+        // console.log(temp);
+        res.send();
     })
-
-    
-
-
-    
-
-    res.send("Server is up and running.");
 })
 
 
